63674447d82cd7b838b21c96429f42e3f4959cea
Anisha Mahajan <anisha.amcse@gmail.com>
2022-12-17 16:27:11 +0530

    Project created

---
+++ Employee.SignIn.System/Employee.SignIn.System/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Employee.SignIn.System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
