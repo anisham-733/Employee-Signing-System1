f28b302887987ba2beef9deed2206dfa0f8c544d
Anisha Mahajan <anisha.amcse@gmail.com>
2022-12-27 11:32:04 +0530

    few changes

--- EmployeeSignInSystem/Employee.SignIn.System/Controllers/GuardController.cs
+++ EmployeeSignInSystem/Employee.SignIn.System/Controllers/GuardController.cs
@@ -49,7 +49,7 @@ namespace EmployeeSignInSystem.Controllers
         {
             ViewBag.FirstName=FirstName; ViewBag.LastName=LastName;
             ViewBag.StartDate=StartDate; ViewBag.EndDate=EndDate;
-            IEnumerable<EmployeeTempBadge> emps = _guardService.GetReport(StartDate., EndDate, FirstName, LastName);
+            IEnumerable<EmployeeTempBadge> emps = _guardService.GetReport(StartDate, EndDate, FirstName, LastName);
 
             return View(emps);
 
